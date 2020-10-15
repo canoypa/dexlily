4c1da5eb83315936a12e3f4fb6bdbe966e953706
Cano <contact.sasasaky@gmail.com>
2020-10-15 09:44:54 +0900

    型定義

--- src/key-path.ts
+++ src/key-path.ts
@@ -1,3 +1,10 @@
-export const keyPath = (name: string, path?: string) => ({
+import { KeyPath } from "./types";
+
+type keyPathCreator = {
+  <T extends string>(name: T, path?: undefined): KeyPath<T, T>;
+  <N extends string, T extends string>(name: N, path: T): KeyPath<N, T>;
+};
+
+export const keyPath: keyPathCreator = (name: string, path?: string) => ({
   keyPath: { [name]: path || name },
 });
--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,7 @@ import { openIDB } from "./open-idb";
 
 export type DBReq = ReturnType<typeof openIDB>;
 
-export type KeyPath = Record<"keyPath", Record<string, string>>;
+export type KeyPath<N extends string, T extends string> = Record<
+  "keyPath",
+  Record<N, T>
+>;
